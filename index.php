1adbb26c77b6c31503030e5250aa623ae253287a
Sara Saccon <sacoon@MBP-di-Sara.fritz.box>
2024-10-07 11:09:03 +0200

    Completato Selfwork 02

--- index.php
+++ index.php
@@ -23,4 +23,18 @@ define("BOOLEAN", $boolean);
 
 
 
+// Selfwork 02
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = "$text2";
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+
+echo $text1 . " " . $text2 . " " . $text3 . $text4 . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
+
 ?>
