76a35f4b6a7eed2cac1848b6ee450b5e57755da5
Sara Saccon <sacoon@MBP-di-Sara.fritz.box>
2024-10-07 11:38:02 +0200

    Selfwork 03

--- index.php
+++ index.php
@@ -32,9 +32,57 @@ $text4 = "?";
 $text5 = "Perchè";
 $text6 = "$text2";
 $text7 = 'bevuto';
-$text8 = "tutto";
+$text8 = "tutto\n";
 
 
 echo $text1 . " " . $text2 . " " . $text3 . $text4 . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
+
+
+
+
+
+
+$words1 = [
+    'vostro',
+    67,
+    'essere',
+    'colle',
+    'mi',
+    'sempre',
+  [
+    'oscura',
+    'era',
+    89,
+    [
+    'mezzo',
+     'E'
+    ],
+  'ritrovai',
+  'per'
+  ],
+  'diritta'
+  ];
+  
+  $words2 = [
+    'elemento1' => 25.89,
+    'elemento2' => 'possa',
+    'elemento3' => [
+      'Virgilio',
+      'Favore',
+      'favore',
+      ['fortuna']
+    ],
+   'fine' => '!'
+  ];
+    
+    $space = " ";
+    $versetto = 'di cammin di nostra vita';
+    $versetto1 = 'una selva';
+    $versetto2 = 'via era smarrita';
+
+  $results = 'Nel' . $space . $words1[6][3][0] . $space . $versetto . $space . $words1[4] . $space .  $words1[6][4] . $space . $words1[6][5] . $space . $versetto1 . $space . $words1[6][0] . ",che' la" . $space . $words1[7] . $space . $versetto2; 
+
+echo $results;
+
 
 ?>
